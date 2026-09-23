7869f6758a5da30841a77d249f22a94c907ff29a
kdbroskii <kdbroskii@gmail.com>
2026-09-23 21:39:24 +0100

    Export screen capture and detection components

--- src/screen/__init__.py
+++ src/screen/__init__.py
@@ -4,5 +4,13 @@ This package only observes/records frames and extracts non-targeting game state.
 """
 
 from .recorder import FrameRecorder, ScreenState, HudPerception
+from .capture import CaptureRegion, WindowsScreenCapture
+from .perception import BoundingBox, TextDetection, PerceptionResult, OCRReader, HudTextInterpreter
+from .detector import YoloDetector
 
-__all__ = ["FrameRecorder", "ScreenState", "HudPerception"]
+__all__ = [
+    "FrameRecorder", "ScreenState", "HudPerception",
+    "CaptureRegion", "WindowsScreenCapture",
+    "BoundingBox", "TextDetection", "PerceptionResult",
+    "OCRReader", "HudTextInterpreter", "YoloDetector",
+]
